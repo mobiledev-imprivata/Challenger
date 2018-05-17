079933eaa358f678c5519523da0e67172fbbce40
Jay Tucker <jtucker@imprivata.com>
2018-05-17 12:53:42 -0400

    Initial commit

---
+++ Challenger/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Challenger
+//
+//  Created by Jay Tucker on 5/17/18.
+//  Copyright © 2018 Imprivata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sendChallenge(_ sender: Any) {
+        print("challenge")
+    }
+    
+}
+
